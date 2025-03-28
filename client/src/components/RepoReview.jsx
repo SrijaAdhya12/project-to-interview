0be1457be5948f6c2cdbbdf7966c7e965096ded0
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2025-03-28 13:40:38 +0530

    feat: added loader

--- client/src/components/RepoReview.jsx
+++ client/src/components/RepoReview.jsx
@@ -71,7 +71,12 @@ const RepoReview = () => {
 	const renderReviewResults = () => {
 		if (loading) {
 			return (
-				<LoadingState/>
+				<div className="flex justify-center items-center h-full min-h-[100px]">
+					<div className="relative w-16 h-16">
+						<div className="absolute inset-0 border-4 border-gray-200 rounded-full animate-ping"></div>
+						<div className="absolute inset-0 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+					</div>
+				</div>
 			)
 		}
 
